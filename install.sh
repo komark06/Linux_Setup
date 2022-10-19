1d160c005d91e2a924a42c48ba6ea196908016f3
komark06 <komark06@gmail.com>
2022-10-19 14:10:32 +0800

    Add undofile to vim

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 Basic_software="vim-gtk build-essential curl git htop glances gh"
 Setup_dir=Linux_Setup
+VimUndo_dir=.vim/undo
 
 # Install basic software
 sudo apt update -y || exit 1
@@ -8,11 +9,12 @@ sudo apt upgrade -y || exit 1
 sudo apt -y install $Basic_software || exit 1
 
 # Setup vim
-if [ ! -d "./Linux_Setup" ]; then
+if [ ! -d "./$Setup_dir" ]; then
 git clone https://github.com/komark06/Linux_Setup.git
 fi
-if [ ! -d "~/.vim" ]; then
-    mkdir -p ~/.vim || exit 1
+if [ ! -d "~/$VimUndo_dir" ]; then
+    mkdir -p ~/$VimUndo_dir || exit 1
 fi
+
 cp -r $Setup_dir/colors ~/.vim || exit 1
 cp .vimrc ~ || exit 1
