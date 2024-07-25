eb99444ce0abf579fbe81596c1f74f86144439a8
komark06 <komark06@gmail.com>
2024-07-25 18:44:37 +0000

    Remove duplicate git clone

--- install.sh
+++ install.sh
@@ -9,13 +9,10 @@ sudo apt upgrade -y || exit 1
 sudo apt -y install $Basic_software || exit 1
 
 # Setup vim
-if [ ! -d "./$Setup_dir" ]; then
-git clone https://github.com/komark06/Linux_Setup.git
-fi
 if [ ! -d "~/$VimUndo_dir" ]; then
     mkdir -p ~/$VimUndo_dir || exit 1
 fi
 
 mkdir -p ~/.vim || exit 1
-cp -r $Setup_dir/colors ~/.vim || exit 1
-cp $Setup_dir/.vimrc ~ || exit 1
+cp -r colors ~/.vim || exit 1
+cp .vimrc ~ || exit 1
