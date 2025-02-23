8324d798c1490c316daba6711d953d1b650377cb
komark06 <komark06@gmail.com>
2025-02-23 19:38:01 +0800

    Correct package name

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-Basic_software="vim-gtk build-essential curl git htop glances gh clang-format valgrind cppcheck linux-tools-common tree"
+Basic_software="vim-gtk3 build-essential curl git htop glances gh clang-format valgrind cppcheck linux-tools-common tree"
 Setup_dir=Linux_Setup
 VimUndo_dir=.vim/undo
 
